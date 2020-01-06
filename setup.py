01b730ea653e6063d926224008443af8e814b89d
Hyeonki Hong <hhk7734@gmail.com>
2020-01-06 17:49:59 +0900

    Use Pybind11 instead of Swig
    close #8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 '''
 MIT License
 
-Copyright (c) 2019 Hyeonki Hong <hhk7734@gmail.com>
+Copyright (c) 2019-2020 Hyeonki Hong <hhk7734@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,21 +23,39 @@ SOFTWARE.
 '''
 
 from setuptools import setup, Extension
-from setuptools.command.sdist import sdist
-from setuptools.command.build_py import build_py
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
 from os import path
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 CHANGELOG_PATH = path.join(BASE_DIR, "debian/changelog")
 
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
 ext_modules = [
     Extension(
         "lot._lot",
-        sources=["wrap/lot.i"],
-        libraries=["lot"],
-        swig_opts=["-c++",
-            "-I/usr/local/include",
-            "-I/usr/include"]
+        sources=["c_src/gpio.cpp"],
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True)
+        ],
+        language="c++"
     ),
 ]
 
@@ -46,25 +64,78 @@ with open(CHANGELOG_PATH, "r") as f:
     version = version.split()
     version = version[1][1:-1]
 
-'''
-for upload
-'''
-class sdist_after_ext(sdist):
-    def run(self):
-        self.run_command("build_ext")
-        return sdist.run(self)
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
 
-'''
-in the distribution when running setup.py bdist or bdist_wheel.
-'''
-class build_py_after_ext(build_py):
-    def run(self):
-        self.run_command("build_ext")
-        return build_py.run(self)
+
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14/17] compiler flag.
+    The newer version is prefered over c++11 (when it is available).
+    """
+    flags = ['-std=c++17', '-std=c++14', '-std=c++11']
+
+    for flag in flags:
+        if has_flag(compiler, flag):
+            return flag
+
+    raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                       'is needed!')
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': ["-llot"],
+    }
+    l_opts = {
+        'msvc': [],
+        'unix': ["-llot"],
+    }
+
+    if sys.platform == 'darwin':
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        c_opts['unix'] += darwin_opts
+        l_opts['unix'] += darwin_opts
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        link_opts = self.l_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' %
+                        self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append('/DVERSION_INFO=\\"%s\\"' %
+                        self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+            ext.extra_link_args = link_opts
+        build_ext.build_extensions(self)
+
 
 setup(
-    version = version,
-    ext_modules = ext_modules,
-    cmdclass = {"sdist": sdist_after_ext,
-                "build_py": build_py_after_ext},
+    version=version,
+    ext_modules=ext_modules,
+    install_requires=['pybind11>=2.4'],
+    setup_requires=['pybind11>=2.4'],
+    cmdclass={'build_ext': BuildExt},
+    zip_safe=False,
 )
