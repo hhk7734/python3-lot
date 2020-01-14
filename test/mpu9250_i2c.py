0d298d2a03f44a9a7a26ae46a61e358cc1782b71
Hyeonki Hong <hhk7734@gmail.com>
2020-01-14 23:12:42 +0900

    Update test code

---
+++ test/mpu9250_i2c.py
@@ -0,0 +1,27 @@
+import lot
+from mpu9250_reg import *
+import struct
+
+mpu = lot.I2c("/dev/i2c-2")
+
+lot.init_time()
+mpu.init()
+
+mpu.write_reg(MPU9250_ADDRESS, MPU9250_PWR_MGMT_1, bytes([MPU9250_H_RESET]))
+lot.delay_ms(100)
+
+mpu.write_reg(MPU9250_ADDRESS, MPU9250_PWR_MGMT_1, bytes([MPU9250_CLKSEL_PLL]))
+mpu.write_reg(MPU9250_ADDRESS, MPU9250_USER_CTRL, bytes([MPU9250_I2C_MST_EN]))
+
+while True:
+    data = mpu.read_reg(MPU9250_ADDRESS, MPU9250_ACCEL_XOUT_H, 6)
+
+    '''
+    XH, XL, YH, YL, ZH, ZL
+    '>': big-endian
+    'h' : int16_t
+    '''
+    xyz = struct.unpack('>hhh', data)
+    print(xyz)
+
+    lot.delay_ms(300)
